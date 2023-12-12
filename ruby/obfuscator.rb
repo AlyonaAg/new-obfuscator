509ff1dc94a96b63a42669bd72c1d96098306f8f
AlyonaAg <gigala2001@gmail.com>
2023-12-13 00:56:53 +0300

    transform const

--- ruby/obfuscator.rb
+++ ruby/obfuscator.rb
@@ -4,7 +4,6 @@ require 'unparser'
 $inner_func_and_var = {}
 $all_identifier = {}
 
-
 def extract_prefix(str)
   match = str.match(/^(@@|@|\$)/)
   match.nil? ? "" : match[0]
@@ -20,33 +19,49 @@ def gen_name
   name
 end
 
+def get_expr(operation, left, right)
+  Parser::AST::Node.new(
+    :begin,
+    [
+      Parser::AST::Node.new(
+        :send,
+        [
+          Parser::AST::Node.new(:int, [left]),
+          operation.to_sym,
+          Parser::AST::Node.new(:int, [right])]
+        )]
+    )
+end
+
 
 def traverse(node, &func_before)
   if func_before.is_a?(Proc)
-    node = func_before.call(node)
-    # return new_node if new_node != node
+    new_node = func_before.call(node)
   end
 
-  new_child = node.children.dup
+  new_child = new_node.children.dup
   new_child.each_with_index do |value, index|
     if value.is_a?(Parser::AST::Node)
       new_subnode = traverse(value, &func_before)
-      if new_subnode != node
+      if new_subnode != new_node
         new_child[index] = new_subnode
       end
     end
   end
-  node = node.updated(node.type, new_child)
+  new_node = new_node.updated(new_node.type, new_child)
 
-  node
+  new_node
 end
 
 def collect_identifier(node)
   collect = lambda do |coll_node|
-    types = [:lvasgn, :gvasgn, :cvasgn, :ivasgn, :arg]
+    types0 = [:lvasgn, :gvasgn, :cvasgn, :ivasgn, :arg, :def]
+    types1 = [:const]
 
-    if types.include?(coll_node.type)
+    if types0.include?(coll_node.type)
       $inner_func_and_var[coll_node.children[0]] = {}
+    elsif types1.include?(coll_node.type)
+      $inner_func_and_var[coll_node.children[1]] = {}
     end
 
     return coll_node
@@ -58,9 +73,10 @@ end
 
 def rename_identifier(node)
   rename = lambda do |rename_node|
-    types = [:lvasgn, :gvasgn, :cvasgn, :ivasgn, :lvar, :arg]
+    types0 = [:lvasgn, :gvasgn, :cvasgn, :ivasgn, :lvar, :arg, :ivar, :cvar, :gvar, :def]
+    types1 = [:send, :const]
 
-    if types.include?(rename_node.type) && $inner_func_and_var.key?(rename_node.children[0])
+    if types0.include?(rename_node.type) && $inner_func_and_var.key?(rename_node.children[0])
       unless $all_identifier.key?(rename_node.children[0])
         prefix = extract_prefix(rename_node.children[0].name)
         $all_identifier[rename_node.children[0]] = prefix + gen_name
@@ -70,6 +86,19 @@ def rename_identifier(node)
       new_child = rename_node.children.dup
       new_child[0] = $all_identifier[rename_node.children[0]].to_sym
       rename_node = rename_node.updated(rename_node.type, new_child)
+    elsif types1.include?(rename_node.type) && $inner_func_and_var.key?(rename_node.children[1])
+      unless $all_identifier.key?(rename_node.children[1])
+        prefix = extract_prefix(rename_node.children[1].name)
+        if rename_node.type == :const
+          prefix = 'C' + prefix
+        end
+        $all_identifier[rename_node.children[1]] = prefix + gen_name
+      end
+
+
+      new_child = rename_node.children.dup
+      new_child[1] = $all_identifier[rename_node.children[1]].to_sym
+      rename_node = rename_node.updated(rename_node.type, new_child)
     end
 
     return rename_node
@@ -78,6 +107,40 @@ def rename_identifier(node)
   traverse(node, &rename)
 end
 
+
+def transform_constants(node)
+  transform = lambda do |transform_node|
+    if transform_node.type == :int && !$transform_const && transform_node.children.length == 1
+      opt = rand(1..7)
+
+      value = transform_node.children[0].to_s.to_i
+      case opt
+      when 1
+        number1 = rand(1..(value + 1)*2)
+        if value < number1
+          number2 = number1 - value
+          return get_expr("-", number1, number2)
+        else
+          number2 = value - number1
+          return get_expr("+", number1, number2)
+        end
+      when 2
+        number1 = rand((value + 1)..((value + 1) * 10))
+        number2 = number1 * rand(1..100) + value
+        return get_expr("%", number2, number1)
+      when 3
+        number1 = rand(1..((value + 1) * 10))
+        number2 = value ^ number1
+        return get_expr("^", number2, number1)
+      end
+    end
+
+    return transform_node
+  end
+
+
+  traverse(node, &transform)
+end
 
 input_filename = ARGV[0]
 output_filename = ARGV[1]
@@ -103,12 +166,12 @@ end
 ast = Unparser.parse(file_content)
 ast = ast.dup
 puts ast
-#traverse(ast)
 ast = collect_identifier(ast)
 ast = rename_identifier(ast)
+ast = transform_constants(ast)
 
 
-puts $inner_func_and_var
+#puts $inner_func_and_var
 result_content = Unparser.unparse(ast)
 puts result_content
 File.open(output_filename, 'w') { |file| file.write(result_content) }
